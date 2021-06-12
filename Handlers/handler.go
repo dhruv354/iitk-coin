6f8c2f14803d1f45069baf6f9ca48deb0f9378d5
dhruv354 <58386850+dhruv354@users.noreply.github.com>
2021-06-12 14:37:14 +0530

    Update handler.go

--- Handlers/handler.go
+++ Handlers/handler.go
@@ -183,27 +183,3 @@ func SignupRoute(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("USer with this rollno created"))
 	}
 }
-
-// /**********************/
-
-// func isUserExists(db *sql.DB, rollno int) bool {
-// 	row := db.QueryRow("SELECT rollno  from User where rollno= ? ", rollno)
-// 	temp := ""
-// 	row.Scan(&temp)
-// 	return temp != ""
-// }
-
-// func insertIntoTable(db *sql.DB, name string, rollno int, password string) {
-// 	fmt.Println("inside insetintotable")
-
-// 	insertStudent_info := `INSERT INTO User(rollno, name, password) VALUES(?, ?, ?)`
-
-// 	insertStudent, err := db.Prepare(insertStudent_info)
-
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		panic(err)
-// 	}
-// 	insertStudent.Exec(rollno, name, password)
-// 	fmt.Println("inserted Student in the table")
-// }
